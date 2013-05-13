7bb8cb529715f898b6cc09a1cd421677ab7e2819
Joker-SMF <makerzzz.zzzz@gmail.com>
2013-05-13 23:12:38 +0530

    licensing thinggy

--- Hiding posts from recent posts/addHooks.php
+++ Hiding posts from recent posts/addHooks.php
@@ -1,7 +1,31 @@
 <?php
-/**************************************************
-	GoogleTagged Mod v2.0 - GoogleTaggged-Integrate.php
-**************************************************/
+
+/**
+ * @package manifest file for Hiding posts from recent posts
+ * @version 1.3
+ * @author Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
+ * @copyright Copyright (c) 2012
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Contributor(s):
+ * Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
+ * Helped in updating the mod for SMF 2.x branch
+ */
+
 if (!defined('SMF'))
 	die('Hacking attempt...');
 
