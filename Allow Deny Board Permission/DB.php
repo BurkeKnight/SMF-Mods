45dfe566ec3279ace53587966960e203c8df32bd
Sid <makerzzz.zzzz@gmail.com>
2013-12-01 17:18:17 +0530

    Allow Deny Board permissions

---
+++ Allow Deny Board Permission/DB.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+* @package manifest file for Allow Deny Board permissions
+* @version 1.0
+* @author Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
+* @copyright Copyright (c) 2012, Siddhartha Gupta
+* @license http://www.mozilla.org/MPL/MPL-1.1.html
+*/
+
+/*
+* Version: MPL 1.1
+*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Initial Developer of the Original Code is
+*  Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
+* Portions created by the Initial Developer are Copyright (C) 2012
+* the Initial Developer. All Rights Reserved.
+*
+* Contributor(s):
+*
+*/
+
+
+// If we have found SSI.php and we are outside of SMF, then we are running standalone.
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+elseif (!defined('SMF')) // If we are outside SMF and can't find SSI.php, then throw an error
+	die('<b>Error:</b> Cannot install - please verify you put this file in the same place as SMF\'s SSI.php.');
+
+	global $smcFunc;
+
+	db_extend('packages');
+
+	$table_columns = $smcFunc['db_list_columns']('{db_prefix}boards');
+
+	$deny_column = array(
+		'name' => 'member_groups_deny',
+		'type' => 'varchar',
+		'size' => 255,
+		'null' => false,
+		'default' => '',
+	);
+
+	// Add the column the new column
+	return $smcFunc['db_add_column']('{db_prefix}boards', $deny_column, null, 'skip_if_exists');
+
+	updateSettings(array('enable_allow_deny' => '1'));
+
+?>
---
+++ Allow Deny Board Permission/readme.txt
@@ -0,0 +1,25 @@
+By default this modification in doesn't make any considerable changes in your forum and your forum will remain as it is.
+
+This modification helps us the admin to create deny permission for each membergroup(regular/post) per board basis.
+
+With the default permission system of SMF if a member is having more than 1 groups assigned to him/her and even if one of the assigned group is having access to the board, in that case the member has complete access to the board.
+This becomes a bit of issue for administrators having a lot of groups on there forums.
+
+Just to make sure that the mod is properly enabled please follow the below mentioned steps:
+Administration Center >> Boards >> Settings 
+- Enable allow/deny mod (make sure this option is checked)
+
+With this mod you can edit deny board from 2 places
+
+1. Go to
+Administration Center >> Boards >> Modify Boards
+- Try to add/modify any board
+- Just check the member groups you want to deny from accessing this board by selecting the checkboxes in "Deny groups:" region.
+
+2. Or
+Administration Center >> Membergroups
+- Try to add or edit a membergroup
+- Just below visible boards you will be able to see deny board permission too.
+
+
+All done, the user having the membergroup checked can't access the board anymore even if the other membergroup associated with the user has the access permission.
